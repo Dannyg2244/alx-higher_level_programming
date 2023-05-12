3875f37a7c9fd5f8b9640421cfeb8b18d97cd2cf
Dannyg2244 <ikuyajolugbenga@gmail.com>
2023-05-12 01:34:33 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,16 @@
 
 if __name__ == "__main__":
 
-    import hidden_4
+    import importlib.util
 
-    names = dir('hidden_4')
+    module_name = "hidden_4"
+    module_spec = importlib.util.spec_from_file_location(module_name,
+                                                         "hidden_4.pyc")
+    module = importlib.util.module_from_spec(module_spec)
+    module_spec.loader.exec_module(module)
+
+    names = [name for name in dir(module) if not name.startswith("__")]
+    names.sort()
+
     for name in names:
-        if (names[:2] != "__"):
-            print(name)
+        print(name)
